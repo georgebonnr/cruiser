f71240cb95c792cf260792890aa67eaaae305fe5
George Bonner <g.e.bonner@gmail.com>
2013-06-26 12:28:37 -0500

    Update gameplay.rb

--- gameplay.rb
+++ gameplay.rb
@@ -6,7 +6,7 @@ locations = [random_no, random_no + 1, random_no + 2]
 gb.set_locations_cells(locations)
 is_alive = true
 while is_alive
-  puts 'Enter a guess between 1 and 10: '
+  puts "Enter a number between 1 and 10! (bad things happen if you don't)"
   STDOUT.flush
   user_guess = gets.chomp
   result = gb.check_yourself(user_guess)
@@ -16,6 +16,8 @@ while is_alive
     print "You have sunk the cruiser! It took you #{no_of_guesses} guesses. "
     if no_of_guesses > 10
       puts "If I were you I wouldn't take command of anything anytime soon."
+    elsif no_of_guesses > 4
+      puts "Congratulations, you are an average quality war criminal."
     elsif no_of_guesses > 3
       puts "Not bad."
     else
